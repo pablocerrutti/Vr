79ee490bb5b4f95ea68133a92d755d3f05a75eca
pablocerrutti <pablocerrutti@gmail.com>
2026-09-11 18:57:16 -0300

    Actualizar cache para nueva brújula y visión nocturna

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='vr-nocturno-v7';
+const CACHE_NAME='vr-nocturno-v8';
 const ASSETS=['./','./index.html','./css/style.css','./js/camera.js','./js/reticle.js','./js/app.js','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
